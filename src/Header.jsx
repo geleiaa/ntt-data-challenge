522c5fddb66d0aae880ab8d655ffdfdbf92f16e5
geleia <geleia@mail.com>
2023-03-03 03:03:49 -0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+            <header className='App-header'>
+                <h2>Header</h2>
+            </header>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const getNaApi = (event) => {
+    console.log('Get na Api', event.target.value);
+}
+
+const Home = () => {
+    return (
+        <>
+            <section>
+                <div>
+                    <h3>Seach Sinopse</h3>
+                    <p>Qualquer texto só pra testar</p>
+                </div>
+                <div className="search">
+                    <input placeholder="Search" className="inpstyle" type='text' />
+                    <button className="btstyle" onClick={e => getNaApi(e)} >Submit</button>
+                    <button className="btstyle" >Reset</button>
+                </div >
+            </section>
+            <section>
+                <div className="movie">
+                    <div className="moviedescr">
+                        <h3>Movie Title</h3>
+                        <p className="mvsinopse" >
+                            Thirty years after the events of Blade Runner (1982), <br />
+                            a new Blade Runner, L.A.P.D. Officer "K" (Ryan Gosling), unearths a long-buried secret that has <br />
+                            the potential to plunge what's left of society into chaos. K's discovery leads him on a quest to find <br />
+                            Rick Deckard (Harrison Ford), a former L.A.P.D. Blade Runner, who has been missing for thirty years. <br /> </p>
+                        <p>Elenco</p>
+                        <p>Review</p>
+                        <button className="btstyle" >Favorite</button>
+                    </div>
+                    <img className="poster" alt="Movie Poster" src="poster.jpg" />
+                </div>
+            </section>
+        </>
+    );
+}
+
+
+export default Home;
